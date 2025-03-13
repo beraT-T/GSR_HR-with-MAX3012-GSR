5b31456904e806f7c06f0fc3f3cd9895258c4901
berat <berat_baki@outlook.com>
2025-03-13 23:15:39 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,19 @@
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 
+#define MQTT_BROKER "broker.hivemq.com"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "esmanur/healthData"
+#define MQTT_USER "hivemq.webclient.1741827367846" 
+#define MQTT_PASS "AS46kt3W0Hqp!RK%b>$a"
+
 #define SECRET_SSID "TurkTelekom_TP7CB8_2.4GHz"		// replace SSID with your WiFi network name
 #define SECRET_PASS "4CM79L9F7Nv9"         // replace Password with your WiFi password
 char ssid[] = SECRET_SSID;   // WiFi SSID
 char pass[] = SECRET_PASS;   // WiFi Password
+
 WiFiClient client;
 PubSubClient mqttClient(client);
-
 MAX30105 particleSensor;
 
 const byte RATE_SIZE = 4; // Ortalama hesaplamak için kullanılacak örnek sayısı
@@ -21,50 +27,34 @@ long lastBeat = 0;
 float beatsPerMinute;
 int beatAvg;              //gönderilecek veriler 1 ortalama nabız 
 
+byte ledBrightness = 0x1F;
+byte sampleAverage = 8;
+byte ledMode = 3;
+int sampleRate = 100;
+int pulseWidth = 411;
+int adcRange = 4096;
+
+unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
 const long interval = 20000; // upload intervali
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Initializing...");
-
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
-    Serial.println("MAX30105 was not found. Please check wiring/power.");
-    while (1);
-  }
-
-#define MQTT_BROKER "broker.hivemq.com"
-#define MQTT_PORT 1883
-#define MQTT_TOPIC "esmanur/healthData"
-#define MQTT_USER "hivemq.webclient.1741827367846" 
-#define MQTT_PASS "AS46kt3W0Hqp!RK%b>$a"
+StaticJsonDocument<200> data;
+String jsonString;
 
 
-
-  byte ledBrightness = 0x1F;
-  byte sampleAverage = 8;
-  byte ledMode = 3;
-  int sampleRate = 100;
-  int pulseWidth = 411;
-  int adcRange = 4096;
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
-
-  WiFi.mode(WIFI_STA);
-  setupMQTT(); // MQTT sunucu ayarlarını başlat
-}
 
 void setupMQTT(){
   mqttClient.setServer(MQTT_BROKER, MQTT_PORT);
 }
 
 void connectToMQTT() {
+  String mqttClientId= "ESPClient";
+
   while (!mqttClient.connected()) {
     Serial.print("MQTT'ye bağlanılıyor...");
-    String mqttClientId= "ESPClient";
     mqttClientId += String(random(0xffff), HEX);
     if (mqttClient.connect("ESP32Client", MQTT_USER, MQTT_PASS)) {
       Serial.println("Bağlandı!");
-      //mqttClient.subscribe (MQTT_TOPIC);
     } else {
       Serial.print("Bağlantı başarısız, hata kodu: ");
       Serial.print(mqttClient.state());
@@ -79,13 +69,15 @@ void publishMessage ()
 if (!mqttClient.connected()) {
   connectToMQTT();       // MQTT bağlantısını kontrol et
 }
-mqttClient.loop();           // MQTT istemcisi döngüsü
+
 
 if (currentMillis - previousMillis >= interval) {
   previousMillis = currentMillis;
   Serial.print("MQTT Mesajı Gönderiliyor: ");
   Serial.println(jsonString);
   mqttClient.publish(MQTT_TOPIC, jsonString.c_str()); // JSON'u MQTT'ye gönder.
+  //Serial.print("MQTT Mesajı Gönderildi "); boolean publish (topic, payload, [length], [retained]) fonksiyonu ile kontorl et
+
   
 }
 }
@@ -120,7 +112,7 @@ double HR(){
   return(beatAvg);
 }
 // wifi bağlantısını kontrol eden kodu direkt loopun içine yazmak yerine bağlantıyı kontrol eden bir fonksiyon oluşturduk
-void checkWiFiConnection() {
+void checkWiFiConnection() {//WİFİ CONNECTİON DİYE BAŞKA BİR FONKSİYON DAHA LAZIM İLK BAĞLANTI İÇİN SETUPA EKLENECEK
   if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Reconnecting to WiFi...");
     WiFi.begin(ssid, pass);
@@ -138,17 +130,36 @@ void checkWiFiConnection() {
   }
   
 }
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
+    Serial.println("MAX30105 was not found. Please check wiring/power.");
+    while (1);
+  }
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+
+  
+  WiFi.mode(WIFI_STA);
+
+  setupMQTT(); // MQTT sunucu ayarlarını başlat
+  connectToMQTT();
+  mqttClient.subscribe (MQTT_TOPIC);
+}
+
+
 void loop() {
 
-  StaticJsonDocument<200> data;
+  mqttClient.loop();           // MQTT istemcisi döngüsü
+
   data["HR"] = HR();
   //data["GSR"] = GSR();
-
-  String jsonString;
   serializeJson(data, jsonString);
   checkWiFiConnection();
-  publishMessage();
-  unsigned long currentMillis = millis();
+  publishMessage();//topic, mesajın içeriği, qos fonksiyonun bağlantısı önce olması lazım mesajı attığımız fonksiyon sadece publish message setupa mqtt connect eklenecek
+  currentMillis = millis();
   
 
 }
