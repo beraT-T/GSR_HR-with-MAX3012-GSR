fea0074a7f0eebd7c5ccb5ceff71e47b1e6bb261
esma <e220504024@stud.tau.edu.tr>
2025-03-19 11:29:35 +0300

    düzenleme

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,18 @@
 #define MQTT_TOPIC "esmanur/healthData"
 #define MQTT_USER "hivemq.webclient.1741827367846" 
 #define MQTT_PASS "AS46kt3W0Hqp!RK%b>$a"
+#define MQTT_QOS 1
+#define MQTT_RETAIN false
 
 #define SECRET_SSID "TurkTelekom_TP7CB8_2.4GHz"		// replace SSID with your WiFi network name
 #define SECRET_PASS "4CM79L9F7Nv9"         // replace Password with your WiFi password
 char ssid[] = SECRET_SSID;   // WiFi SSID
 char pass[] = SECRET_PASS;   // WiFi Password
 
-WiFiClient client;
-PubSubClient mqttClient(client);
+WiFiClient wificlient;
+PubSubClient mqttClient(wificlient);
 MAX30105 particleSensor;
-
+ 
 const byte RATE_SIZE = 4; // Ortalama hesaplamak için kullanılacak örnek sayısı
 byte rates[RATE_SIZE];    // Kalp atış hızlarını saklamak için dizi
 byte rateSpot = 0;
@@ -51,7 +53,24 @@ const long interval = 20000; // upload intervali
 StaticJsonDocument<200> data;
 String jsonString;
 
+void setupWiFi(){
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
 
+  int attempts =0;
+
+  while (WiFi.status() != WL_CONNECTED && attempts<10){
+    delay(1000);
+    Serial.print(".");
+    attempts++;
+  }
+  if (WiFi.status() != WL_CONNECTED){
+    Serial.println("Connected!");
+  }
+  else{
+    Serial.println("Failed to connect to WiFi");
+  }
+}
 
 void setupMQTT(){
   mqttClient.setServer(MQTT_BROKER, MQTT_PORT);
@@ -74,27 +93,12 @@ void connectToMQTT() {
   }
 }
 
-void publishMessage () 
-{
-if (!mqttClient.connected()) {
-  connectToMQTT();       // MQTT bağlantısını kontrol et
+void checkWiFiConnection() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Wİfi connection lost. Reconnecting...");
+    setupWiFi();
+  }
 }
-
-
-if (currentMillis - previousMillis >= interval) {
-  previousMillis = currentMillis;
-  Serial.print("MQTT Mesajı Gönderiliyor: ");
-  Serial.println(jsonString);
-  mqttClient.publish(MQTT_TOPIC, jsonString.c_str()); // JSON'u MQTT'ye gönder.
-  //Serial.print("MQTT Mesajı Gönderildi "); boolean publish (topic, payload, [length], [retained]) fonksiyonu ile kontorl et
-
-  
-}
-}
-// gsr example kodundan da buraya gsr değerini return eden fonksiyon eklenecek 
-//double GSR(){
-  //return(1000);
- //}
 
 //HR fonksiyonu burada sadece ortalama nabzı hesaplayıyor
 double HR(){
@@ -121,25 +125,34 @@ double HR(){
   Serial.println(beatAvg);
   return(beatAvg);
 }
-// wifi bağlantısını kontrol eden kodu direkt loopun içine yazmak yerine bağlantıyı kontrol eden bir fonksiyon oluşturduk
-void checkWiFiConnection() {//WİFİ CONNECTİON DİYE BAŞKA BİR FONKSİYON DAHA LAZIM İLK BAĞLANTI İÇİN SETUPA EKLENECEK
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Reconnecting to WiFi...");
-    WiFi.begin(ssid, pass);
-    int attempt = 0;
-    while (WiFi.status() != WL_CONNECTED && attempt < 10) { // 10 kere denesin
-      delay(1000);
-      Serial.print(".");
-      attempt++;
-    }
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Connected!");
-    } else {
-      Serial.println("Failed to connect.");
-    }
-  }
-  
+
+void publishMessage () 
+{
+if (!mqttClient.connected()) {
+  connectToMQTT();       // MQTT bağlantısını kontrol et
 }
+
+if (currentMillis - previousMillis >= interval) {
+  previousMillis = currentMillis;
+
+  data.clear();
+  data["HR"]= HR();
+  //GSR için de eklenecek
+
+  serializeJson(data, jsonString); //?
+  Serial.print("Publishing MQTT message; ");
+  Serial.println(jsonString);
+
+  if(mqttClient.publish(MQTT_TOPIC, jsonString.c_str(), jsonString.length(), MQTT_QOS, MQTT_RETAIN)){
+    Serial.print("Message published successfully.");
+  } 
+  else{
+    Serial.println("Failed to publish message.");
+  }
+  //Serial.print("MQTT Mesajı Gönderildi "); boolean publish (topic, payload, [length], [retained]) fonksiyonu ile kontorl et
+}
+}
+
 
 void setup() {
   Serial.begin(115200);
@@ -151,25 +164,17 @@ void setup() {
   }
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 
-  
-  WiFi.mode(WIFI_STA);
-
-  setupMQTT(); // MQTT sunucu ayarlarını başlat
+  setupWiFi();
+  setupMQTT(); 
   connectToMQTT();
   mqttClient.subscribe (MQTT_TOPIC);
 }
 
 
 void loop() {
-
-  mqttClient.loop();           // MQTT istemcisi döngüsü
-
-  data["HR"] = HR();
-  //data["GSR"] = GSR();
-  serializeJson(data, jsonString);
-  checkWiFiConnection();
-  publishMessage();//topic, mesajın içeriği, qos fonksiyonun bağlantısı önce olması lazım mesajı attığımız fonksiyon sadece publish message setupa mqtt connect eklenecek
   currentMillis = millis();
-  
 
-}
+  checkWiFiConnection();
+  mqttClient.loop();           // MQTT istemcisi döngüsü
+  publishMessage();//topic, mesajın içeriği, qos fonksiyonun bağlantısı önce olması lazım mesajı attığımız fonksiyon sadece publish message setupa mqtt connect eklenecek
+ }
